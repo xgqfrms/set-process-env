90d605adf1c8a250b9bd063325d9eab894cf7431
xgqfrms <xgqfrms@outlook.com>
2021-10-14 13:30:33 +0800

    cross-env

---
+++ test.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+
+// PORT_ENV=8090 && node ./test.js
+
+// console.log('process =', process);
+// console.log('process.env =', process.env);
+
+console.log('PORT_ENV =', process.env.PORT_ENV);
+console.log('NODE_ENV =', process.env.NODE_ENV);
+
+/*
+
+PORT_ENV = undefined
+NODE_ENV = undefined
+
+*/
+
+
+// export PORT_ENV=8090 && node ./test.js
+
+
+/*
+
+PORT_ENV = 8090
+NODE_ENV = undefined
+
+*/
+
+// check windows or mac ???
+console.log('process.arch =', process.arch);
+console.log('process.platform =', process.platform === 'darwin' ? 'macOS' : 'Windows');
+// arch: 'x64',
+// platform: 'darwin',
+
+
+// ❌
+// ./node_modules/cross-env/dist/bin/cross-env.js PORT_ENV=8090 && node ./test.js
+
+// ✅
+// ./node_modules/cross-env/src/bin/cross-env.js PORT_ENV=8090 && node ./test.js
+
+
+/*
+
+PORT_ENV = 8090
+NODE_ENV = undefined
+
+*/
