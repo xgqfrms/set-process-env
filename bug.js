cb12a9cb0b9fdaeaa3f38124aabf881ee9b4db4c
xgqfrms <xgqfrms@outlook.com>
2021-10-14 18:05:53 +0800

    shell.exec(command); bug
    https://github.com/xgqfrms/set-process-env/issues/3

---
+++ bug.js
@@ -0,0 +1,10 @@
+
+
+
+
+// ./node_modules/shelljs/bin/shjs PORT_ENV=8090 && node ./test.js
+
+
+// ./node_modules/shelljs/src/exec.js
+
+// node_modules/shelljs/shell.js
---
+++ spe-bug.js
@@ -0,0 +1,33 @@
+const shell = require("shelljs");
+
+const key = 'PORT_ENV';
+const env = 8888;
+
+// const command = `export ${key}=${env}`;
+// const command = `set ${key}=${env}`;
+
+// 命令 & 参数 分隔符号
+// const command = 'export "PORT_ENV=8888"';
+const command = 'export PORT_ENV=8888';
+
+// shelljs 执行 command
+
+shell.exec('whoami');
+shell.exec('which sh');
+shell.exec('which bash');
+// shell.exec('pwd');
+// shell.exec('ls -al');
+shell.exec(command);
+// shell.exec('export', 'PORT_ENV=8888');
+
+// ??? （追加）写入到 .zshrc 文件
+// export PORT_ENV="8888"
+// source ~/.profile
+// source ~/.bash_profile
+
+// export ZSH="/Users/xgqfrms/.oh-my-zsh"
+
+
+
+console.log('\n command =', command);
+console.log('\n PORT_ENV =', process.env.PORT_ENV);
